74ba5638e0fbe063a5b8876f68bc388b848221d2
B-Perederei <woncaru@gmail.com>
2026-02-17 11:31:02 +0200

    Progress made

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Define paths correctly without the broken $ variables
-# // means search subdirectories recursively
 export TEXINPUTS=".:./style-classes//:./tex//:"
 export BIBINPUTS=".:./tex//:"
 export BSTINPUTS=".:./tex//:"
 
-# Run latexmk
 latexmk -xelatex -output-directory=build -interaction=nonstopmode tex/thesis.tex
 wslview ./build/thesis.pdf
 
