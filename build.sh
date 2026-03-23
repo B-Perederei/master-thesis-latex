339a1863cae8ca5ca7a492d4b322213f8d6ceb40
Bohdan Perederei <72132841+B-Perederei@users.noreply.github.com>
2026-03-23 11:10:52 +0200

    Version of Master Thesis after Research Practice (#1)
    First Version of Master Thesis

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ export BSTINPUTS=".:./tex//:"
 
 latexmk -xelatex -output-directory=build -interaction=nonstopmode tex/thesis.tex
 wslview ./build/thesis.pdf
-
+# cp ./build/thesis.pdf "/mnt/c/Users/Bohdan/Desktop/Перша версія МД.pdf"
