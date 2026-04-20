7ef05505bc0e32cb770dc8c39fe7f38ac66efecd
B-Perederei <woncaru@gmail.com>
2026-04-20 17:25:02 +0300

    Added code listings

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ export TEXINPUTS=".:./style-classes//:./tex//:"
 export BIBINPUTS=".:./tex//:"
 export BSTINPUTS=".:./tex//:"
 
+rm ./build/thesis.pdf
 latexmk -xelatex -output-directory=build -interaction=nonstopmode tex/thesis.tex
 wslview ./build/thesis.pdf
 # cp ./build/thesis.pdf "/mnt/c/Users/Bohdan/Desktop/Перша версія МД.pdf"
